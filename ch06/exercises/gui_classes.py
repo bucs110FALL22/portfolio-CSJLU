d94041079f959869d63d3444603c8aebdc721b74
Johnny Lu <112282001+CSJLU@users.noreply.github.com>
2022-10-24 21:59:01 +0000

    gui_class

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,17 @@
+class Block:
+  def __init__(self):
+    self.shape = square
+    self.generate_coin = 1
+    self.color = brown
+
+class Player:
+  def __init__(self):
+    self.shape = human
+    self.size = 10
+    self.speed = 5
+
+class Goomba:
+  def __init__(self):
+    self.shape = mushroom
+    self.color = brown
+    self.size = 5
